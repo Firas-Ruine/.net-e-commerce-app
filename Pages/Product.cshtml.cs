a50cc93ac2413dc42c0581be74f7d9478dfa562e
Firas Ruine <fruine6@gmail.com>
2024-02-11 17:45:25 +0100

    implementing product page logic ad UI

--- Pages/Product.cshtml.cs
+++ Pages/Product.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using e_commerce.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -19,16 +20,34 @@ namespace e_commerce.Pages
 
         // Property to hold the list of products
         public List<Product> Products { get; set; }
+        public List<string> StoreNames { get; set; } = new List<string>(); // Initialize the list
 
         // Handler method for HTTP GET requests
-        public void OnGet()
+        public void OnGet(int? productId)
         {
             // Retrieve products with related category, brand, and stocks information
-            Products = _ecommerceContext.Products
+            IQueryable<Product> productsQuery = _ecommerceContext.Products
                 .Include(p => p.Category)
                 .Include(p => p.Brand)
                 .Include(p => p.Stocks)
-                .ToList();
+                .Include(p => p.OrderItems)
+                .Include(p => p.Stocks)
+                    .ThenInclude(s => s.Store);
+
+            // Check if productId parameter is provided
+            if (productId.HasValue)
+            {
+                // Filter products by productId
+                productsQuery = productsQuery.Where(p => p.ProductId == productId);
+            }
+
+            // Execute the query and assign the result to Products property
+            Products = productsQuery.ToList();
+
+            // Populate StoreNames with store names
+            StoreNames = _ecommerceContext.Stores.Select(s => s.StoreName).ToList();
         }
+
     }
-}
+
+    }
